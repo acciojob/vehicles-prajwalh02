1d91ad24d7125a9e6574b8719027e3232e3af3a6
Prajwal hole <99700885+prajwalh02@users.noreply.github.com>
2023-08-14 19:40:22 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -4,11 +4,14 @@ public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
-        super(name, 4, 2, 7, isManual, "Formula 1", 1);
+        super(name,4,1,9,isManual,"Racing",1);
     }
 
     public void accelerate(int rate){
-        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
+        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        int currSpeed=super.getCurrentSpeed();
+        newSpeed= currSpeed+rate;
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -24,24 +27,25 @@ public class F1 extends Car {
             stop();
             changeGear(1);
         }
-        //for all other cases, change the gear accordingly
-        else if(newSpeed >=0  && newSpeed <= 50) {
+        else if (newSpeed>=1 && newSpeed<=50) {
             changeGear(1);
         }
-        else if(newSpeed >= 51 && newSpeed <= 100) {
+        else if (newSpeed>=51 && newSpeed<=100) {
             changeGear(2);
-        } else if (newSpeed >= 101 && newSpeed <= 150) {
+        }
+        else if (newSpeed>=101 && newSpeed<=150) {
             changeGear(3);
         }
-        else if(newSpeed >= 151 && newSpeed <= 200) {
+        else if (newSpeed>=151 && newSpeed<=200) {
             changeGear(4);
         }
-        else if(newSpeed >= 201 && newSpeed <= 250)  {
+        else if (newSpeed>=201 && newSpeed<=250) {
             changeGear(5);
         }
-        else if(newSpeed > 250) {
+        else if(newSpeed>250){
             changeGear(6);
         }
+        //for all other cases, change the gear accordingly
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
